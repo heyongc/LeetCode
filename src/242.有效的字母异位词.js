54eb2b116838e24a118a2fff4d77b18d4dc3665d
herman <528195677@qq.com>
2024-07-12 21:22:47 +0800

    [242] 有效的字母异位词

---
+++ src/242.有效的字母异位词.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=javascript
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  let sMap = new Map();
+
+  for (let c of s) {
+    sMap.set(c, (sMap.get(c) || 0) + 1);
+  }
+
+  for (let c of t) {
+    if (sMap.has(c)) {
+      sMap.set(c, sMap.get(c) - 1);
+      if (sMap.get(c) === 0) sMap.delete(c);
+    } else {
+      return false;
+    }
+  }
+
+  return sMap.size === 0;
+};
+// @lc code=end
