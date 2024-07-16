7899a0edb6133c48d75d448bd6fcac40209e608d
herman <528195677@qq.com>
2024-07-16 22:59:59 +0800

    [102] 二叉树的层序遍历

---
+++ src/102.二叉树的层序遍历.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=javascript
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+// 时间复杂度 O(n)
+var levelOrder = function (root) {
+  if (!root) return [];
+
+  const queue = [root];
+  const result = [];
+
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+
+    for (let i = 0; i < levelSize; i++) {
+      const currentNode = queue.shift();
+      currentLevel.push(currentNode.val);
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+
+    result.push(currentLevel);
+  }
+
+  return result;
+};
+// @lc code=end
