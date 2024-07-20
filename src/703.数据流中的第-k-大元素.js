86156712ef7005534d589f6d82b48e73a11f0235
herman <528195677@qq.com>
2024-07-20 22:18:14 +0800

    [703] 数据流中的第 K 大元素 - 最小堆法

--- src/703.数据流中的第-k-大元素.js
+++ src/703.数据流中的第-k-大元素.js
@@ -10,9 +10,90 @@
  * @param {number[]} nums
  */
 // 排序法
+// var KthLargest = function (k, nums) {
+//   this.k = k;
+//   this.nums = nums;
+// };
+
+// /**
+//  * @param {number} val
+//  * @return {number}
+//  */
+// KthLargest.prototype.add = function (val) {
+//   this.nums.push(val);
+//   this.nums.sort((a, b) => b - a);
+//   return this.nums[this.k - 1];
+// };
+
+// 最小堆法
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(val) {
+    this.heap.push(val);
+    this.bubbleUp(this.heap.length - 1);
+  }
+
+  bubbleUp(index) {
+    while (index > 0) {
+      let parent = Math.floor((index - 1) / 2);
+      if (this.heap[parent] <= this.heap[index]) break;
+      [this.heap[parent], this.heap[index]] = [
+        this.heap[index],
+        this.heap[parent],
+      ];
+      index = parent;
+    }
+  }
+
+  extractMin() {
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown(0);
+    return min;
+  }
+
+  bubbleDown(index) {
+    while (true) {
+      let leftChild = 2 * index + 1;
+      let rightChild = 2 * index + 2;
+      let smallest = index;
+
+      if (
+        leftChild < this.heap.length &&
+        this.heap[leftChild] < this.heap[smallest]
+      ) {
+        smallest = leftChild;
+      }
+      if (
+        rightChild < this.heap.length &&
+        this.heap[rightChild] < this.heap[smallest]
+      ) {
+        smallest = rightChild;
+      }
+      if (smallest === index) break;
+
+      [this.heap[smallest], this.heap[index]] = [
+        this.heap[index],
+        this.heap[smallest],
+      ];
+      index = smallest;
+    }
+  }
+}
+
 var KthLargest = function (k, nums) {
   this.k = k;
-  this.nums = nums;
+  this.minHeap = new MinHeap();
+  nums.forEach((num) => {
+    this.minHeap.insert(num);
+  });
+
+  while (this.minHeap.heap.length > k) {
+    this.minHeap.extractMin();
+  }
 };
 
 /**
@@ -20,9 +101,13 @@ var KthLargest = function (k, nums) {
  * @return {number}
  */
 KthLargest.prototype.add = function (val) {
-  this.nums.push(val);
-  this.nums.sort((a, b) => b - a);
-  return this.nums[this.k - 1];
+  if (this.minHeap.heap.length < this.k) {
+    this.minHeap.insert(val);
+  } else if (val > this.minHeap.heap[0]) {
+    this.minHeap.insert(val);
+    this.minHeap.extractMin();
+  }
+  return this.minHeap.heap[0];
 };
 
 /**
@@ -31,3 +116,10 @@ KthLargest.prototype.add = function (val) {
  * var param_1 = obj.add(val)
  */
 // @lc code=end
+
+// let kkk = new KthLargest(1, []);
+// console.log(kkk.add(-3));
+// console.log(kkk.add(-2));
+// console.log(kkk.add(-4));
+// console.log(kkk.add(0));
+// console.log(kkk.add(4));
