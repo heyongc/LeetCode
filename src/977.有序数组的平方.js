393f3821afdbcc51a50fe2ea5ac0a4a4bd93c0f4
herman <528195677@qq.com>
2024-08-03 23:17:56 +0800

    [977] 有序数组的平方

---
+++ src/977.有序数组的平方.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=977 lang=javascript
+ *
+ * [977] 有序数组的平方
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  let k = nums.length;
+
+  let result = new Array(k);
+
+  for (let left = 0, right = k - 1; left <= right; ) {
+    if (nums[left] * nums[left] < nums[right] * nums[right]) {
+      result[--k] = nums[right] * nums[right];
+      right--;
+    } else {
+      result[--k] = nums[left] * nums[left];
+      left++;
+    }
+  }
+  return result;
+};
+// @lc code=end
