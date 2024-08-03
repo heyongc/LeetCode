c80ff3393602fe9a759e2a02172dec947ada6e52
herman <528195677@qq.com>
2024-08-03 23:39:01 +0800

    [977] 有序数组的平方

--- src/977.有序数组的平方.js
+++ src/977.有序数组的平方.js
@@ -9,20 +9,28 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// var sortedSquares = function (nums) {
+//   let k = nums.length;
+
+//   let result = new Array(k);
+
+//   for (let left = 0, right = k - 1; left <= right; ) {
+//     if (nums[left] * nums[left] < nums[right] * nums[right]) {
+//       result[--k] = nums[right] * nums[right];
+//       right--;
+//     } else {
+//       result[--k] = nums[left] * nums[left];
+//       left++;
+//     }
+//   }
+//   return result;
+// };
+
+// 暴力解法 双重循环
 var sortedSquares = function (nums) {
-  let k = nums.length;
-
-  let result = new Array(k);
-
-  for (let left = 0, right = k - 1; left <= right; ) {
-    if (nums[left] * nums[left] < nums[right] * nums[right]) {
-      result[--k] = nums[right] * nums[right];
-      right--;
-    } else {
-      result[--k] = nums[left] * nums[left];
-      left++;
-    }
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] *= nums[i];
   }
-  return result;
+  return nums.sort((a, b) => a - b);
 };
 // @lc code=end
